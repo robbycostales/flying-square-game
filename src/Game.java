a3615607f9e648898e73a647a18c387001e2358b
Robby Costales <rcostales15@simons-rock.edu>
2017-04-25 17:13:49 -0400

    first

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,41 @@
+import java.util.Random;
+
 public class Game {
     private Grid grid;
     private int userRow;
+    private int userCol;
     private int msElapsed;
     private int timesGet;
     private int timesAvoid;
 
+    public static final int NUMCOL = 10;
+    public static final int NUMROW = 10;
+
+    public static final int downPRESS = 40;
+    public static final int upPRESS = 38;
+    // reciprocal of new image appearance frequency
+    public static final int FREQ = 10;
+
+    public Random rand = new Random();
+    public int choice;
+    public int randomRow;
+
     public Game(int rows, int cols) {
+        // grid stores and displays images
         grid = new Grid(rows, cols);
+        // userRow keeps track of user's row on left edge of grid
         userRow = 0;
+        // userCol keeps track of user's col
+        userCol = 0;
+        // since start of game
         msElapsed = 0;
         timesGet = 0;
         timesAvoid = 0;
         updateTitle();
-        grid.setImage(new Location(userRow, 0), "user.gif");
+        grid.setImage(new Location(userRow, userCol), "user.gif");
     }
 
+    // main game method
     public void play() {
         while (!isGameOver()) {
             grid.pause(100);
@@ -28,10 +49,53 @@ public class Game {
         }
     }
 
-    public void handleKeyPress() {
+    public void handleKeyPress()
+    {
+        int key = grid.checkLastKeyPressed();
+        switch(key)
+        {
+            // if up-key
+            case upPRESS:
+                if(userRow == 0)
+                {
+                    break;
+                } else
+                {
+                    grid.setImage(new Location(userRow, userCol), null);
+                    userRow -= 1;
+                    grid.setImage(new Location(userRow, userCol), "user.gif");
+                }
+                break;
+            // if down-key
+            case downPRESS:
+                if(userRow == NUMROW-1)
+                {
+                    break;
+                } else
+                {
+                    grid.setImage(new Location(userRow, userCol), null);
+                    userRow += 1;
+                    grid.setImage(new Location(userRow, userCol), "user.gif");
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     public void populateRightEdge() {
+        choice = rand.nextInt(FREQ);
+        randomRow = rand.nextInt(NUMCOL-1);
+        switch(choice){
+            case 0:
+                grid.setImage(new Location(randomRow, NUMCOL-1), "avoid.gif");
+                break;
+            case 1:
+                grid.setImage(new Location(randomRow, NUMCOL-1), "get.gif");
+                break;
+            default:
+                break;
+        }
     }
 
     public void scrollLeft() {
@@ -53,7 +117,7 @@ public class Game {
     }
 
     public static void test() {
-        Game game = new Game(5,10);
+        Game game = new Game(NUMROW, NUMCOL);
         game.play();
     }
 
